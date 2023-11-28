5f27817740e4440273772c8b90a35bd09ef23572
Franco De Giovannini <franco.dg90@gmail.com>
2023-11-28 13:33:54 +1100

    change download weights from hf-cli to hub-hb

--- download_models.py
+++ download_models.py
@@ -2,6 +2,7 @@ import os
 
 import yaml
 
+from huggingface_hub import HfApi
 from langchain.embeddings import HuggingFaceEmbeddings
 from omegaconf import OmegaConf
 
@@ -13,11 +14,13 @@ LLM = CONFIG.llm.download_args
 VS = CONFIG.vectorstore.model
 
 ## Download LLAMA model
-os.system(
-    f"huggingface-cli download {LLM.size} "
-    + f"{LLM.name} "
-    + "--local-dir . "
-    + "--local-dir-use-symlinks False"
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+hf = HfApi()
+hf.hf_hub_download(
+    repo_id=LLM.size,
+    filename=LLM.name,
+    local_dir= ".",
+    local_dir_use_symlinks=False
 )
 os.system(f"mv {LLM.name} llm.gguf")
 
